abd24ca295327bd74870bfa7eebb700833acef75
sebastian <sebastian.hartwig@uni-ulm.de>
2022-09-08 16:57:06 +0200

    update setup.py

--- pointnet2/setup.py
+++ pointnet2/setup.py
@@ -6,13 +6,17 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import glob
+import os.path as osp
+import os
 
+this_dir = osp.dirname(osp.abspath(__file__))
 _ext_src_root = "_ext_src"
 _ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
     "{}/src/*.cu".format(_ext_src_root)
 )
 _ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
 
+os.environ["TORCH_CUDA_ARCH_LIST"] = "3.7+PTX;5.0;6.0;6.1;6.2;7.0;7.5"
 setup(
     name='pointnet2',
     ext_modules=[
@@ -20,12 +24,14 @@ setup(
             name='pointnet2._ext',
             sources=_ext_sources,
             extra_compile_args={
-                "cxx": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
-                "nvcc": ["-O2", "-I{}".format("{}/include".format(_ext_src_root))],
+                "cxx": ["-O3"],
+                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
             },
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")]
         )
     ],
     cmdclass={
         'build_ext': BuildExtension
-    }
+    },
+    include_package_data=True
 )
