2ea2e66a778f2b26afad444c41b32397291d98ce
sebastian <sebastian.hartwig@uni-ulm.de>
2022-09-14 09:30:27 +0200

    update ckpt loading

--- evaluate.py
+++ evaluate.py
@@ -8,12 +8,6 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from scatterplot.cluster_separation_dataset import ClusterSeparationDataset, ClusterSeparatonDatasetConfig
 
-def parse_ckpt(path):
-    assert Path(path).is_file(), "path must be a ckpt file!"  
-    ckpt = [p for p in Path(path).glob("**/*") if p.suffix == ".ckpt"][0].as_posix()
-    print("Found checkpoint ", ckpt)
-    return ckpt
-
 if __name__ == '__main__':
     parser = ArgumentParser('Evaluate scatterplot model')
     parser.add_argument('--worker', default=8, type=int, help='Number of workers for data loader')
@@ -26,9 +20,8 @@ if __name__ == '__main__':
         devices=1,
     )
 
-    ckpt = parse_ckpt(args.ckpt)
-
-    model = VoteNetModule.load_from_checkpoint(ckpt)
+    assert Path(args.path).is_file(), "Not a valid .ckpt file provided: {}".format(args.ckpt)
+    model = VoteNetModule.load_from_checkpoint(args.ckpt)
 
     test_dataset   = ClusterSeparationDataset(path=args.dataset_path, split="test", num_points=model.opt.n_points)
 
