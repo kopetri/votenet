58f4bcfbdcbc3845cfdf7ef5651d2aee5f455788
sebastian <sebastian.hartwig@uni-ulm.de>
2022-09-15 13:23:24 +0200

    fix eval path

--- evaluate.py
+++ evaluate.py
@@ -45,15 +45,15 @@ if __name__ == '__main__':
     results = trainer.predict(model=model, dataloaders=test_loader)
     for result in results:
         img_gt, img_pred, pid, points, gt_centers, pred_centers = result[0], result[1], result[2], result[3], result[4], result[5]
-        path_gt     = Path(ckpt).parent/"results"/"gt_{}.jpg".format(pid)
-        path_pred   = Path(ckpt).parent/"results"/"pred_{}.jpg".format(pid)
+        path_img_gt     = Path(ckpt).parent/"results"/"gt_{}.jpg".format(pid)
+        path_img_pred   = Path(ckpt).parent/"results"/"pred_{}.jpg".format(pid)
         path_points = Path(ckpt).parent/"results"/"points_{}.npy".format(pid)
         path_gt     = Path(ckpt).parent/"results"/"gt_{}.npy".format(pid)
         path_pred   = Path(ckpt).parent/"results"/"pred_{}.npy".format(pid)
         path_gt.parent.mkdir(parents=True, exist_ok=True)
         
-        cv2.imwrite(path_gt.as_posix(), img_gt)
-        cv2.imwrite(path_pred.as_posix(), path_pred)
+        cv2.imwrite(path_img_gt.as_posix(), img_gt)
+        cv2.imwrite(path_img_pred.as_posix(), img_pred)
         np.save(path_points, points)
         np.save(path_gt, gt_centers)
         np.save(path_pred, pred_centers)
