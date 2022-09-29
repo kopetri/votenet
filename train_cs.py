7b247315db49a3f0930a24d255fc4930fb566dbb
sebastian <sebastian.hartwig@uni-ulm.de>
2022-09-29 09:22:12 +0200

    remove unused args

--- train_cs.py
+++ train_cs.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     trainer.add_argument('--weight_decay', default=0.1, type=float, help='Add learning rate decay.')
     trainer.add_argument('--use_augmentation', action='store_true', help="Whether to use data augmentation")
     trainer.add_argument('--n_points', default=500, type=int, help="Whether to use data augmentation")
-    trainer.add_argument('--sampling', default='vote_fps', type=str, help="sampling strategy")
     trainer.add_argument('--max_clusters', default=1, type=int, help="Number of clusters to separate.")
 
     args = trainer.setup()
